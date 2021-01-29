8f6e6aade94c3fd676f3dc2acd185205ad68c7fc
pavelklak <qaz789wsx>
2021-01-29 09:53:59 +0300

    jazzMusic, RapMusic added

---
+++ src/main/java/ru/klakotckii/springproject/JazzMusic.java
@@ -0,0 +1,11 @@
+package ru.klakotckii.springproject;
+
+/**
+ * @author pavelklak
+ */
+public class JazzMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Lui Armstrong";
+    }
+}
--- src/main/java/ru/klakotckii/springproject/MusicPlayer.java
+++ src/main/java/ru/klakotckii/springproject/MusicPlayer.java
@@ -1,17 +1,48 @@
 package ru.klakotckii.springproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author pavelklak
  */
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private int volume;
+    private String name;
+    private String duration;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public int getVolume() {
+        return volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList){
+            System.out.println("Playing: " + music.getSong() + "Duration: " + getDuration());
+        }
     }
 }
